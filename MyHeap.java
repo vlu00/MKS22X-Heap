ff0b11082ab7418b0285a5f70a20dcc670fbd9cf
vlu00 <vlu00@stuy.edu>
2019-04-29 14:54:58 -0400

    Working on pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,18 @@
 public class MyHeap{
-  private static void pushDown(int[]data. int size, int index) {
+  private static void pushDown(int[]data, int size, int index) {
     while (data[index] < data[2*index+1] || data[index] < data[2*index+2]) {
-
+      if (data[2*index+1] > data[2*index+2]) {
+        int temp = data[index];
+        data[index] = data[2*index+1];
+        data[2*index+1] = temp;
+        index++;
+      }
+      else {
+        int temp = data[index];
+        data[index] = data[2*index+2];
+        data[2*index+2] = temp;
+        index += 2;
+      }
     }
   }
 }
