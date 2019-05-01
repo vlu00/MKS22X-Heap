4df93ae7491418237da1ebf92ee3bcd092b84f71
vlu00 <vlu00@stuy.edu>
2019-05-01 00:39:59 -0400

    Added missing parenthesis and pushDown works

--- MyHeap.java
+++ MyHeap.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class MyHeap{
   private static void pushDown(int[]data, int size, int index) {
-    while (2*index+2 < size && data[index] < data[2*index+1] || data[index] < data[2*index+2]) {
+    while (2*index+2 < size && (data[index] < data[2*index+1] || data[index] < data[2*index+2])) {
       if (data[2*index+1] > data[2*index+2]) {
         int temp = data[index];
         data[index] = data[2*index+1];
@@ -20,32 +20,9 @@ public class MyHeap{
   }
 
   public static void main(String[] args) {
-    int[] A = new int[]{3, 4, 2};
-    pushDown(A, 3, 0);
+    int[] A = new int[]{1, 16, 12, 8, 6, 7};
+    System.out.println(Arrays.toString(A));
+    pushDown(A, 6, 0);
     System.out.println(Arrays.toString(A));
   }
 }
-    /*
-    boolean rightspot = false;
-    while (!rightspot) {
-      if (2*index+2 > size) {
-        rightspot = true;
-      }
-      else if (data[2*index+1] > data[2*index+2]) {
-        if (data[index] < data[2*index+1]) {
-          int temp = data[index];
-          data[index] = data[2*index+1];
-          data[2*index+1] = temp;
-          index++;
-        }
-      }
-      else {
-        int temp = data[index];
-        data[index] = data[2*index+2];
-        data[2*index+2] = temp;
-        index += 2;
-      }
-    }
-  }
-}
-*/
